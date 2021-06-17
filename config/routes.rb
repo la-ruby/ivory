53c89b4ee16dd5a1f21ea728e307b4641a2be3cb
Sam W <sam@sam-we.com>
2021-06-17 05:52:49 -0700

    Proof of concept

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/one'
+  get 'pages/two'
+  get 'pages/three'
+
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
